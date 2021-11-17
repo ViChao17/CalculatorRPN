79a35fd2a109b5b4b9bb9e2ddbc53789098c0b0c
Виктор <vitek_zema@mail.ru>
2021-11-18 01:18:19 +0300

    good start

---
+++ main.py
@@ -0,0 +1,107 @@
+from flask import render_template, url_for, request
+from flask import Flask
+menu = []
+
+app = Flask(__name__)
+
+
+priority = {
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 2,
+    '^': 3,
+    '(': 0
+}
+
+
+def is_number(num):
+    for c in num:
+        if c in '1234567890.':
+            continue
+        return False
+    return True
+
+
+def parse_input_string(input_str: str) -> list[str]:
+    number = ''
+    output: list[str] = []
+    for c in input_str:
+        if c.isdigit() or c == '.':
+            number += c
+            continue
+        if len(number) > 0:
+            output.append(number)
+            number = ''
+        output.append(c)
+    if len(number) != 0:
+        output.append(number)
+
+    return output
+
+
+def to_polish_notation(input_list: list[str]):
+    stack: list = []
+    output = []
+    for el in input_list:
+        if is_number(el):
+            output.append(float(el))
+            continue
+        if len(stack) == 0:
+            stack.append(el)
+            continue
+        elif el in '*/+-^':
+            stack_top = stack[-1]
+            if priority[el] <= priority[stack_top]:
+                output.append(stack.pop())
+                stack.append(el)
+            else:
+                stack.append(el)
+            continue
+        elif el == '(':
+            stack.append('(')
+        elif el == ')':
+            stack_top = stack.pop()
+            while stack_top != '(':
+                output.append(stack_top)
+                stack_top = stack.pop()
+
+    while len(stack) > 0:
+        output.append(stack.pop())
+
+    return output
+
+
+def calc(rpn: list) -> float:
+    stack = []
+    for el in rpn:
+        if type(el) == float:
+            stack.append(el)
+        else:
+            right: float = stack.pop()
+            left: float = stack.pop()
+            if el == '/':
+                stack.append(left/right)
+            if el == '*':
+                stack.append(left*right)
+            if el == '-':
+                stack.append(left-right)
+            if el == '+':
+                stack.append(left+right)
+            if el == '^':
+                stack.append(left**right)
+    return stack[0]
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    value = 0
+    if request.method == "POST":
+        parsed_input_string = parse_input_string(request.form['expression'])
+        rpn_list = to_polish_notation(parsed_input_string)
+        value = calc(rpn_list)
+    return render_template('index.html', value=value)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
